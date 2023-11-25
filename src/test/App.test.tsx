dfff0b1608396ab6fd7aa1be45d19a274506c2e5
fabianoLiberatori <fabiano29585@gmail.com>
2023-11-25 11:39:37 -0300

    fix: removendo imports nao utilizados

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
-import { vi, describe, test, expect } from 'vitest';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { describe, test } from 'vitest';
 import App from '../App';
 import NewsProvider from '../context/NewsProvider';
 import { BrowserRouter } from 'react-router-dom';
